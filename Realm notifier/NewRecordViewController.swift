0cf500b4070cafb22f7cdd89eabb9e7903511d23
Alex Shcherbinin <mendeleev174@icloud.com>
2023-04-07 02:01:46 +0500

    Добавлено удаление и редактирование существующих записей с обновлением в базу

--- Realm notifier/NewRecordViewController.swift
+++ Realm notifier/NewRecordViewController.swift
@@ -6,13 +6,41 @@
 //
 
 import UIKit
+import RealmSwift
 
 class NewRecordViewController: UIViewController {
 
+    @IBOutlet weak var titleLbl: UITextField!
+    
+    @IBOutlet weak var currentDateLbl: UITextField!
+    
+    @IBOutlet weak var noteLbl: UITextField!
+    
+    
+    @IBAction func pressSaveAndReturn(_ sender: UIButton) {
+        if isUpdate {
+            guard titleLbl.text?.count != 0 else { return }
+            delegate?.updateRecord(titleToUpdate: titleLbl.text!, noteToUpdate: noteLbl.text, sender: realmObject!)
+            navigationController?.popViewController(animated: true)
+        } else {
+            guard readyToSave() else { return }
+            delegate?.saveNewRecord(realmObject!)
+            navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    var realmObject: Notes? = nil
+    
+    weak var delegate: NewRecordViewControllerDelegate?
+    
+    var isUpdate = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        formFill()
+        
     }
     
 
@@ -26,4 +54,31 @@ class NewRecordViewController: UIViewController {
     }
     */
 
+    func formFill() {
+        
+        guard let objToFill = realmObject else {
+            realmObject = Notes()
+            realmObject?.date = NSDate()
+            currentDateLbl.text = NSDate().description
+            return }
+        titleLbl.text = objToFill.name
+        currentDateLbl.text = objToFill.date.description
+        noteLbl.text = objToFill.text
+        
+    }
+    
+    func readyToSave() -> Bool {
+        if titleLbl.text?.count != 0 {
+            realmObject?.name = titleLbl.text!
+            realmObject?.text = noteLbl.text
+            return true
+        }
+        return false
+    }
+    
+}
+
+protocol NewRecordViewControllerDelegate: AnyObject {
+    func saveNewRecord(_ objectToSave: Notes)
+    func updateRecord (titleToUpdate text: String, noteToUpdate note: String?, sender objectToUpdate: Notes)
 }
--- Realm notifier/TableViewController.swift
+++ Realm notifier/TableViewController.swift
@@ -12,13 +12,16 @@ class TableViewController: UITableViewController {
 
     @IBAction func PressAdd(_ sender: UIBarButtonItem) {
         let addVC = storyboard?.instantiateViewController(withIdentifier: "NewVC")
-        navigationController?.pushViewController(addVC!, animated: true)
+        let addVC2 = addVC as! NewRecordViewController
+        addVC2.delegate = self
+        navigationController?.pushViewController(addVC2, animated: true)
     }
     
     let realm = try! Realm()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -53,26 +56,39 @@ class TableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
             // Delete the row from the data source
+            let toDoDelete = realm.objects(Notes.self)
+            try! realm.write {
+                realm.delete(toDoDelete[indexPath.row])
+            }
+            // следующую строку важно ставить в конце условия, так как после удаления строки в таблице, она обновляется с новым количеством строк из базы данных
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let addVC = storyboard?.instantiateViewController(withIdentifier: "NewVC")
+        let addVC2 = addVC as! NewRecordViewController
+        let objectToSend = realm.objects(Notes.self)
+        addVC2.realmObject = objectToSend[indexPath.row]
+        addVC2.isUpdate = true
+        addVC2.delegate = self
+        navigationController?.pushViewController(addVC2, animated: true)
+    }
     /*
     // Override to support rearranging the table view.
     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
@@ -98,4 +114,23 @@ class TableViewController: UITableViewController {
     }
     */
 
+}
+
+extension TableViewController: NewRecordViewControllerDelegate {
+    
+    func saveNewRecord(_ objectToSave: Notes) {
+        try! realm.write {
+            realm.add(objectToSave)
+        }
+        tableView.reloadData()
+    }
+    
+    func updateRecord(titleToUpdate text: String, noteToUpdate note: String?, sender objectToUpdate: Notes) {
+        try! realm.write {
+            objectToUpdate.name = text
+            objectToUpdate.text = note
+        }
+        tableView.reloadData()
+    }
+    
 }
